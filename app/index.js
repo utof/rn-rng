6b85e7cbe3dcb0488ce5b2743d079c26a037b7a5
utof <bondar030@mail.ru>
2023-09-17 20:56:55 +0300

    reinstall all; add folders from tutorial

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'; // to view, and view the text prob?
+import "expo-router/entry"; // what is this?
+
+
+const Home = () => { //why -- its the Home function that will view a text "Home". At the end u export it
+
+return <View>
+    <Text>Je</Text>
+</View>
+}
+
+export default Home;
