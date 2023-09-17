438606128eb22537111920d208c564ee70abc5f8
utof <bondar030@mail.ru>
2023-09-17 20:27:08 +0300

    first commit

---
+++ RN-rng/_layout.js
@@ -0,0 +1,7 @@
+import { Stack } from 'expo-router'; // why?
+
+const Layout = () => { // what is this?
+    return <Stack />;
+}
+
+export default Layout; // why?
---
+++ RN-rng/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import "expo-router/entry"; // what is this?
+
+
+const Home = () => { //why
+
+return <View>
+    <Text>Home</Text>
+</View>
+}
+
+export default Home;
